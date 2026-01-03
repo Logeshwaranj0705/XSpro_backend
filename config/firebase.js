381d2b5c9fdd3c39ecc795ac6414db96e1f76794
logesh_07_05 <naru8sasu9344@gmail.com>
2026-01-03 15:53:44 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,11 @@
 const admin = require("firebase-admin");
-const path = require("path");
-
-const serviceAccountPath = path.join(__dirname, "firebase-service-account.json"); 
 
 try {
-  const serviceAccount = require(serviceAccountPath);
+  if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT env variable not set");
+  }
+
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -12,7 +13,7 @@ try {
 
   console.log("✅ Firebase Admin Initialized");
 } catch (err) {
-  console.error("❌ Firebase Admin initialization failed:", err);
+  console.error("❌ Firebase Admin initialization failed:", err.message);
 }
 
 module.exports = admin.firestore();
