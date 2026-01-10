6ad261bf6fd13bc6da8d3f6fddadef41968de898
logesh_07_05 <naru8sasu9344@gmail.com>
2026-01-10 16:11:08 +0530

    Delete models/CronLock.js

--- models/CronLock.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const cronLockSchema = new mongoose.Schema({
-  name: { type: String, unique: true },
-  lastRunDate: String,
-});
-
-module.exports = mongoose.model("CronLock", cronLockSchema);
